cf30aeba02b427dbffc6b6d867b94d4896a22eaf
Soren Nelson <sorennelson33@gmail.com>
2022-10-29 12:53:58 -0400

    A2 Tests

--- skeleton/tests.py
+++ skeleton/tests.py
@@ -127,3 +127,94 @@ class TestOperators:
 
         output = self.__pull(sink)
         assert all([x.tuple in ans for x in output]) and len(ans) == len(output)
+
+    def __get_query_1(self, ff, mf, uid, mid, tp):
+        sc1 = Scan(ff, None, track_prov=tp)
+        sc2 = Scan(mf, None, track_prov=tp)
+
+        se1 = Select([sc1], None, lambda x: x.tuple[0] == uid, track_prov=tp)
+        se2 = Select([sc2], None, lambda x: x.tuple[1] == mid, track_prov=tp)
+
+        join = Join([se1], [se2], None, 1, 0, track_prov=tp)
+        avg = GroupBy([join], None, 2, 3, lambda x: sum(x) / len(x), track_prov=tp)
+        root_op = Project([avg], None, [1], pull=True, track_prov=tp)
+
+        return root_op
+
+    def __get_query_2(self, ff, mf, uid, tp, pp):
+        sc1 = Scan(ff, None, table_name='Friends', track_prov=tp, propagate_prov=pp)
+        sc2 = Scan(mf, None, table_name='Ratings', track_prov=tp, propagate_prov=pp)
+
+        se1 = Select([sc1], None, lambda x: x.tuple[0] == uid, track_prov=tp, propagate_prov=pp)
+
+        join = Join([se1], [sc2], None, 1, 0, track_prov=tp, propagate_prov=pp)
+        avg = GroupBy([join], None, 2, 3, lambda x: sum(x) / len(x), agg_fun_name='AVG', track_prov=tp, propagate_prov=pp)
+
+        order = OrderBy([avg], None, lambda x: x.tuple[1], ASC=False, track_prov=tp, propagate_prov=pp)
+        limit = TopK([order], None, 1, track_prov=tp, propagate_prov=pp)
+
+        root_op = Project([limit], None, [0], 
+                        track_prov=tp, propagate_prov=pp)
+
+        return root_op, order, [sc1, sc2]
+                    
+    def test_lineage(self):
+        tp = True
+        pp = False
+        ff = '../data/friends-test.txt'
+        mf = '../data/movie_ratings-test.txt'
+        lineage = 0
+        uid = 1190
+
+        root_op, _, _ = self.__get_query_2(ff, mf, uid, tp, pp)
+        tuples = self.__pull(root_op)
+        assert len(tuples) > lineage
+
+        output = tuples[lineage].lineage(is_start=False)
+        assert [t.tuple for t in output] == [(1190, 6), (1190, 1095), (6, 2, 5), (1095, 2, 5)]
+
+    def test_where(self):
+        tp = True
+        ff = '../data/friends-test.txt'
+        mf = '../data/movie_ratings-test.txt'
+        where_row = 0
+        where_attr = 0
+        uid = 1190
+        mid = 0
+
+        root_op = self.__get_query_1(ff, mf, uid, mid, tp)
+        tuples = self.__pull(root_op)
+        assert len(tuples) > where_row
+
+        output = tuples[where_row].where(where_attr, is_start=False)
+        assert str(output) == str([('movie_ratings-test.txt', 7, (6, 0, 4), 4), ('movie_ratings-test.txt', 10, (1095, 0, 5), 5)])
+
+    def test_how(self):
+        tp = False
+        pp = True
+        ff = '../data/friends-test.txt'
+        mf = '../data/movie_ratings-test.txt'
+        how = 0
+        uid = 1190
+
+        root_op, _, _ = self.__get_query_2(ff, mf, uid, tp, pp)
+        tuples = self.__pull(root_op)
+        assert len(tuples) > how
+
+        output = tuples[how].how()
+        assert output == "AVG( (f4*r9@5), (f3*r12@5) )"
+
+    def test_responsibility(self):
+        tp = False
+        pp = True
+        ff = '../data/friends-test.txt'
+        mf = '../data/movie_ratings-test.txt'
+        responsibility = 0
+        uid = 1190
+
+        root_op, order, scans = self.__get_query_2(ff, mf, uid, tp, pp)
+        tuples = self.__pull(root_op)
+        assert len(tuples) > responsibility
+
+        output = tuples[responsibility].responsible_inputs(order, scans)
+        assert str(output) == str([((1190, 6), 0.5), ((6, 2, 5), 0.5), ((1190, 1095), 0.5), ((1095, 2, 5), 0.5)])
